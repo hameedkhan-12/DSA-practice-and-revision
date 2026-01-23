ddffa03b5519c138d9b43ce47d59bd0c37f324da
Hameed Khan <hameed404404@gmail.com>
2026-01-23 08:15:05 +0500

    Revision

--- src/dsa_revision/arrays_easy.ts
+++ src/dsa_revision/arrays_easy.ts
@@ -75,20 +75,102 @@
 // }
 // console.log(bestTimeToSellAndBuyBetter([7, 1, 5, 3, 6, 4]))
 
-const bestTimeToSellAndBuyOpt = (prices: number[]) => {
+// const bestTimeToSellAndBuyOpt = (prices: number[]) => {
+//   let left = 0;
+//   let right = 1;
+//   let maxProfit = 0;
+//   while (left < right) {
+//     if (prices[right] > prices[left]) {
+//       const profit = prices[right] - prices[left];
+//       maxProfit = Math.max(maxProfit, profit);
+//     } else {
+//       left = right;
+//     }
+//     right++;
+//   }
+//   return maxProfit;
+// };
+
+// const containsDup = (nums: number[]) => {
+//   for (let i = 0; i< nums.length; i++){
+//     for(let j = i +1; j<nums.length; j++){
+//       if(nums[i] === nums[j]) return true;
+//     }
+//   }
+// }
+
+// console.log(containsDup([1,2,3,1]))
+
+// const containsDuplBetter = (nums: number[]) => {
+//   const map = new Map<number, number>();
+//   for(let num of nums){
+//     const freq = map.set(num, (map.get(num) || 0) + 1);
+//     if(freq.get(num)! > 1) return true
+//   }
+//   return false
+// };
+
+// console.log(containsDuplBetter([1,2,3]))
+
+// const containsDublOpt = (nums: number[]) => {
+//   const seen = new Set<number>();
+//   for(let num of nums){
+//     if(seen.has(num)) return true;
+//     seen.add(num);
+//   }
+//   return false
+// }
+
+// console.log(containsDublOpt([1,2,3,1]))
+
+// const maxSubArrSum = (arr: number[]) => {
+//     let maxSum = -Infinity;
+//     for(let i=0; i<arr.length; i++){
+//         let curSum = 0;
+//         for(let j = i; j<arr.length; j++){
+//             curSum+= arr[j]
+//         }
+//         maxSum = Math.max(curSum, maxSum)
+//     }
+
+//     return maxSum;
+// }
+
+// console.log(maxSubArrSum([1,5,-3,-4,3,7,1,-2,3]))
+
+// const maxSubArrSumOpt = (nums: number[]) => {
+//   let maxSum = nums[0];
+//   let curSum = 0;
+//   for (let num of nums) {
+//     curSum += num;
+//     maxSum = Math.max(maxSum, curSum);
+
+//     if (curSum < 0) {
+//       curSum = 0;
+//     }
+//   }
+//   return maxSum;
+// };
+// console.log(maxSubArrSumOpt([1, 5, -3, -4, 3, 7, 1, -2, 3, 1]));
+// console.log(maxSubArrSumOpt([1, 2, 3, 4]));
+
+// let i = 0;
+// let j = 2
+// let nums: number[] = [10,20,30,40];
+// [nums[i], nums[j]] = [nums[j], nums[i]]
+
+// console.log(nums)
+
+const removeDuplicates = (nums: number[]): number => {
   let left = 0;
-  let right = 1;
-  let maxProfit = 0;
-  while (left < right) {
-    if (prices[right] > prices[left]) {
-      const profit = prices[right] - prices[left];
-      maxProfit = Math.max(maxProfit, profit);
-    } else {
-      left = right;
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[right] !== nums[left]) {
+      left++;
+      nums[left] = nums[right];
     }
-    right++;
   }
-  return maxProfit;
+
+  return left + 1;
 };
 
-
+console.log(removeDuplicates([1,2,2,3,5,5,7]))
